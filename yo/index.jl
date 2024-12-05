78acf2f5b3bdf147a398afb057d33d0046d58c38
1s22s1 <1s22s1@users.noreply.github.com>
2024-12-05 22:29:03 +0900

    :up:

--- yo/index.jl
+++ yo/index.jl
@@ -1,20 +1,27 @@
 using Plots
 
-grid = nothing
+life_grid = nothing
 height = nothing
 width = nothing
 
 function main()
     global height, width = parseints()
-    global grid = reduce(hcat, [parseints() for _ ∈ 1:height])
+    file_name = parsestring()
+    global life_grid = reduce(hcat, [parseints() for _ ∈ 1:height])
 
-    animation = @animate for i = 1:3
-       heatmap(grid, colorbar=false, ticks=false, axis=false)
+    animation = @animate for i = 1:100
+        heatmap(
+            life_grid,
+            colorbar = false,
+            ticks = false,
+            axis = false,
+            fillcolor = cgrad(:greenpink),
+        )
 
-       next()
+        next()
     end
 
-    gif(animation, "life_game.gif", fps = 10)
+    gif(animation, "$(file_name).gif", fps = 1000)
 end
 
 function next()
@@ -25,24 +32,27 @@ function next()
 
         for x ∈ [1, 0, -1]
             for y ∈ [1, 0, -1]
-                if 1 ≤ i+x ≤ height && 1 ≤ j+y ≤ width && grid[i+x, j+y] == 1
+                if 1 ≤ i + x ≤ height &&
+                   1 ≤ j + y ≤ width &&
+                   !(x == 0 && y == 0) &&
+                   life_grid[i+x, j+y] == 1
+
                     count += 1
                 end
             end
         end
 
-        count -= 1
-
-        if grid[i, j] == 1 && count ∈ [2, 3]
+        if life_grid[i, j] == 1 && count ∈ [2, 3]
             next_grid[i, j] = 1
-        elseif grid[i, j] == 0 && count == 3
+        elseif life_grid[i, j] == 0 && count == 3
             next_grid[i, j] = 1
         end
     end
 
-    global grid = deepcopy(next_grid)
+    global life_grid = deepcopy(next_grid)
 end
 
 parseints() = readline() |> split |> x -> parse.(Int, x)
+parsestring() = readline()
 
 main()
