7604aa7912694530616c178b691dd56c23e7e40b
1s22s1 <1s22s1@users.noreply.github.com>
2024-12-07 08:18:48 +0900

    :up:

--- 20241207/abc197_c.jl
+++ 20241207/abc197_c.jl
@@ -5,20 +5,41 @@ function main()
     n = parseint()
     an = parseints()
 
-    result = 0
+    result = typemax(Int)
 
     for pattern ∈ IterTools.subsets(1:n-1)
+        arr = split_array(an, pattern)
+        arr = map(sub -> reduce(|, sub), arr)
 
-        @show pattern
-        arr = []
-
-        if isempty(pattern)
-            push!(arr, an)
-        else
-        end
+        result = min(result, reduce(⊻, arr))
     end
 
     println(result)
+end
+
+"""
+    split_array(array::Vector{Int}, indexes::Vector{Int})::Vector{Vector{Int}}
+
+インデックスで指定した位置で配列を分割する。
+"""
+function split_array(array::Vector{Int}, indexes)::Vector{Vector{Int}}
+    result = []
+
+    if isempty(indexes)
+        push!(result, array)
+
+        return result
+    end
+
+    push!(result, array[begin:indexes[begin]])
+
+    for i in 2:length(indexes)
+        push!(result, array[indexes[i-1]+1:indexes[i]])
+    end
+
+    push!(result, array[indexes[end]+1:end])
+
+    return result
 end
 
 parseint() = readline() |> x -> parse(Int, x)
